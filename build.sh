02b5ab7bc665dacdcfc5e318f9b10e58b1882d17
Justin Kühner <kuehner@ferrari-electronic.de>
2025-03-31 15:08:49 +0200

    build.sh: publish as single file

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 dotnet clean
 dotnet build
-dotnet publish src/kiota/kiota.csproj -c Release -o ./bin/kiot --framework net9.0
+dotnet publish src/kiota/kiota.csproj -c Release -p:PublishSingleFile=true -o ./bin/kiota --framework net9.0
 #cp -r bin/kiota ../../ferrari/flutter/microsoft_graph_client
